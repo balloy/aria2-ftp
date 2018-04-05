51d36d48f580a168055fe92dd4d147f56f23dc77
Ken Yang <balloy@gmail.com>
2018-04-05 18:59:06 -0500

    allow running a paticular test file, not all.

--- internals/scripts/RunTests.js
+++ internals/scripts/RunTests.js
@@ -1,9 +1,18 @@
 import spawn from 'cross-spawn';
 import path from 'path';
 
-const pattern = process.argv[2] === 'e2e'
-  ? 'test/e2e/.+\\.spec\\.js'
-  : 'test/(?!e2e/)[^/]+/.+\\.spec\\.js$';
+const patternParameter = process.argv[2];
+let pattern;
+if (patternParameter && patternParameter.indexOf('.js') > 0) {
+  // specify one test file
+  pattern = patternParameter.replace(/\\/g, '/');
+} else if (patternParameter === 'e2e') {
+  // all e2e tests
+  pattern = 'test/e2e/.+\\.spec\\.js';
+} else {
+  // all normal tests
+  pattern = 'test/(?!e2e/)[^/]+/.+\\.spec\\.js$';
+}
 
 const result = spawn.sync(
   path.normalize('./node_modules/.bin/jest'),
