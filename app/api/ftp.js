21e3b73dafcac98b050bc72c31bc38cfd91267eb
Ken Yang <balloy@gmail.com>
2018-10-06 14:12:16 -0500

    fix an issue that can't support FTP folder with spaces. (limitation of jsftp.ls, use CMD+MLSD instead to fix the issue, MLSD also yields more accurate modifiedTime)

--- app/api/ftp.js
+++ app/api/ftp.js
@@ -9,6 +9,92 @@ import { getFileType, TYPE_FOLDER } from '../utils/fileType';
 const { URL } = require('url');
 const Jsftp = require('jsftp');
 
+const once = require('once');
+
+// add mlsd support
+Jsftp.prototype.mlsd = function mlsd(path, callback) {
+  const self = this;
+  let listing = '';
+  callback = once(callback);
+
+  self.getPasvSocket( (error, socket) => {
+    if (error) {
+      return callback(error);
+    }
+
+    socket.setEncoding('utf8');
+    socket.on('data', data => { listing += data; });
+
+    self.pasvTimeout(socket, callback);
+
+    socket.once('close', err => {
+      // Split lines
+      const data = listing.split('\r\n');
+      const list = [];
+      for (let i = 0; i < data.length; ++i) {
+        const line = data[i].split(';');
+        // console.log('line: ', line);
+
+        // Remove empty item, . and ..
+        if (line.length >= 7 && line[0] !== 'type=cdir' && line[0] !== 'type=pdir') {
+          /*
+            sample line format:
+            [ 'type=dir',
+              'sizd=13',
+              'modify=20181006014050',
+              'UNIX.mode=0775',
+              'UNIX.uid=1000',
+              'UNIX.gid=1000',
+              'unique=49g65ca',
+              ' [EAC][180926] Summer Pockets Original SoundTrack [FLAC+CUE+LOG+TIF]'
+            ]
+            sample target format:
+            [{
+              name: 'sample_folder',
+              type: 1,
+              time: 1538842500000,
+              size: '12',
+             },
+            ]
+          */
+
+          const str2date = timeVal => {
+            const p = timeVal.match(/^(\d{4})(\d{2})(\d{2})(\d{2})(\d{2})(\d{2})(\.\d+)?$/);
+            const millis = p[7] ? p[7] : 0;
+            return Date.UTC(p[1], p[2] - 1, p[3], p[4], p[5], p[6], millis);
+          };
+
+          list.push({
+            name: line[7].substr(1),  // remove first space
+            type: line[0] === 'type=dir' ? 1 : 0,
+            time: str2date(line[2].replace('modify=', '')),
+            size: line[1].replace('size=', '').replace('sizd=', '')
+          });
+        }
+      }
+      callback(err, list);
+    });
+
+    socket.once('error', callback);
+
+    const cmdCallback = (err, res) => {
+      if (err) {
+        return callback(err);
+      }
+
+      const expectedMarks = {
+        marks: [150, 226],
+        ignore: 226
+      };
+      const isExpectedMark = expectedMarks.marks.some( mark => mark === res.code );
+      if (!isExpectedMark) {
+        callback(new Error(`Expected marks ${expectedMarks.toString()} instead of: ${res.text}`));
+      }
+    };
+    self.execute(`MLSD ${path || ''}`, cmdCallback);
+  });
+};
+
 export const connect = url => {
   const p = new URL(url);
   console.log('ftp url:', p);
@@ -20,7 +106,18 @@ export const connect = url => {
       pass: p.password || '@anonymous',
     });
 
-    ftpClient.on('connect', () => resolve(ftpClient));
+    ftpClient.on('connect', () => {
+      // get Feature List after connection established,
+      // since we prefer to use MLSD so need to know whether it's supported.
+      ftpClient.getFeatures((err) => {
+        if (err) {
+          reject(err.message);
+        } else {
+          resolve(ftpClient);
+        }
+      });
+    });
+
     ftpClient.on('error', err => {
       console.error(err);
       reject(err.message);
@@ -79,10 +176,34 @@ const resolveItems = (resolve, dir, rawItems, timeDiff) => {
   resolve({ dir, items });
 };
 
-export const readDir = (ftpClient, inputDir) => new Promise((resolve, reject) => {
+export const readDir = (ftpClient, inputDir) => {
   const dir = inputDir || '/';
 
   console.log(`Begin loading FTP folder: ${dir}`);
+  const func = ftpClient.hasFeat('mlsd') ? readDirMLSD : readDirLS;
+  return func(ftpClient, dir);
+};
+
+const readDirMLSD = (ftpClient, dir) => new Promise((resolve, reject) => {
+  console.log('Using MLSD');
+  ftpClient.raw('CWD', dir, error => {
+    if (error) {
+      console.warn(error);
+      return reject(error.message);
+    }
+
+    ftpClient.mlsd('', (error2, items) => {
+      if (error2) {
+        console.warn(error2);
+        return reject(error2.message);
+      }
+      return resolveItems(resolve, dir, items, 0);
+    });
+  });
+});
+
+const readDirLS = (ftpClient, dir) => new Promise((resolve, reject) => {
+  console.log('Using LS');
   ftpClient.ls(dir, (error, items) => {
     if (error) {
       console.warn(error);
